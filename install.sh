23fe05e1bdfa5335eb4a6e88c0ec7d752a33f291
Daniel Johansen <drdj@hotmail.dk>
2024-01-27 00:03:20 +0100

    .

--- install.sh
+++ install.sh
@@ -111,8 +111,8 @@ echo -e "\n\tAll LIMA applications have been installed\n\n\n"
 
 # Run Self Test
 # check cuda works
-$program_dir"/build/LIMA_ENGINE/engine_self_test"
-if [ $? -ne 0s ]; then
+$program_dir"/build/code/LIMA_ENGINE/engine_self_test"
+if [ $? -ne 0 ]; then
     echo "engine_self_test failed"
     exit 1
 fi

5fbcd0090ffdb5906ae489b52d0e53f9aa56edd2
Daniel Johansen <drdj@hotmail.dk>
2025-01-28 15:51:36 +0100

    Update the mdrun program to also use the arg parser Rename some args in mdrun and em

--- code/LIMA/mdrun.h
+++ code/LIMA/mdrun.h
@@ -1,112 +1,16 @@
 #include <iostream>
 #include <format>
 #include <filesystem>
-
 #include <CommandlineUtils.h>
 
 #include "Environment.h"
+#include "argparser.h"
+
 namespace fs = std::filesystem;
 
-struct MdrunSetup {
-    MdrunSetup(int argc, char** argv) : work_dir(fs::current_path()) { // TODO: This should use the same function as the other programs
-        for (int i = 2; i < argc; ++i) {
-            std::string arg = CmdLineUtils::ToLowercase(argv[i]);
 
-            if (arg == "-conf") {
-                if (i + 1 < argc) {
-                    conf = work_dir / argv[++i];
-                }
-                else {
-                    std::cerr << "-conf expects a path argument." << std::endl;
-                    exit(1);
-                }
-            }
-            else if (arg == "-topology") {
-                if (i + 1 < argc) {
-                    topol = work_dir / argv[++i];
-                }
-                else {
-                    std::cerr << "-topology expects a path argument." << std::endl;
-                    exit(1);
-                }
-            }
-            else if (arg == "-simparams") {
-                if (i + 1 < argc) {
-                    simpar = work_dir / argv[++i];
-                }
-                else {
-                    std::cerr << "-simparams expects a path argument." << std::endl;
-                    exit(1);
-                }
-            }
-            else if (arg == "-structure_out") {
-                if (i + 1 < argc) {
-                    conf_out = work_dir / argv[++i];
-                }
-                else {
-                    std::cerr << "-structure_out expects a path argument." << std::endl;
-                    exit(1);
-                }
-            }
-            else if (arg == "-trr") {
-                if (i + 1 < argc)
-                    trajOut = work_dir / argv[++i];
-                else {
-                    std::cerr << "-trr expects a path argument." << std::endl;
-                    exit(1);
-                }
-            }
-            else if (arg == "-help" || arg == "-h") {
-                std::cout << helpText;
-                exit(0);
-            }
-            else if (arg == "-display" || arg == "-d")
-                envmode = Full;
-            else {
-                std::cerr << "Unknown argument: " << arg << std::endl;
-                exit(1);
-            }
-        }
-    }
 
-    EnvMode envmode = ConsoleOnly;
-    fs::path work_dir;
-    fs::path conf = work_dir / "conf.gro";
-    fs::path topol = work_dir / "topol.top";
-    fs::path simpar = work_dir / "sim_params.txt";
-    fs::path conf_out = work_dir / "out.gro";
-    std::optional<fs::path> trajOut = std::nullopt;
 
-private:
-    const std::string helpText = R"(
-Usage: mdrun [OPTIONS]
-
-Description:
-    This program runs a molecular dynamics simulation based on provided configuration files and parameters.
-
-Options:
-    -conf [path]
-        Path to the configuration (.gro) file. Defaults to molecule/conf.gro.
-    
-    -topology [path]
-        Path to the topology (.top) file. Defaults to molecule/topol.top.
-    
-    -simparams [path]
-        Path to the simulation parameters file. Defaults to sim_params.txt.
-
-    -display, -d
-        Flag to enable the display, rendering the simulation and displaying information such as temperature, step and more.
-
-    -structure_out [path]
-        Output path for the resulting structure file. Defaults to out.gro.
-
-    -help, -h
-        Display this help text and exit.
-
-Example:
-    mdrun -conf myconf.gro -topology mytopol.top -simparams params.txt -display
-    )";
-};
 
 void PrintTiming(const std::chrono::duration<double> enginetime, double totalNsSimulated) {
     const double wall_time_sec = enginetime.count();
@@ -124,12 +28,64 @@ void PrintTiming(const std::chrono::duration<double> enginetime, double totalNsS
 }
 
 int mdrun(int argc, char** argv) {
-    MdrunSetup setup(argc, argv);
-    auto env = std::make_unique<Environment>(setup.work_dir, setup.envmode);
 
-    const SimParams ip(setup.simpar);
-    GroFile grofile{ setup.conf };
-    TopologyFile topfile{ setup.topol };
+    const std::string helpText = R"(
+Usage: mdrun [OPTIONS]
+
+Description:
+    This program runs a molecular dynamics simulation based on provided configuration files and parameters.
+
+Options:
+    -conf [path]
+        Path to the configuration (.gro) file. Defaults to ./conf.gro
+    
+    -topology [path]
+        Path to the topology (.top) file. Defaults to ./topol.top
+    
+    -simparams [path]
+        Path to the simulation parameters file. Defaults to ./sim_params.txt
+
+    -display, -d
+        Flag to enable the display, rendering the simulation and displaying information such as temperature, step and more
+
+    -conf_out [path]
+        Output path for the resulting structure file. Defaults to ./out.gro
+
+    -trajectory, -trr [path]
+		Output path for the trajectory file. Defaults to not outputting a trajectory file
+
+    -help, -h
+        Display this help text and exit.
+
+Example:
+    mdrun -conf myconf.gro -topology mytopol.top -simparams params.txt -display
+    )";
+
+    ArgParser parser(helpText);
+
+    fs::path work_dir;
+    fs::path conf = work_dir / "conf.gro";
+    fs::path topol = work_dir / "topol.top";
+    fs::path simpar = work_dir / "sim_params.txt";
+    fs::path conf_out = work_dir / "out.gro";
+    fs::path trajOut{};
+
+    bool render = false;
+
+    parser.AddOption({ "-conf", "-c" }, false, conf);
+	parser.AddOption({ "-topology", "-top", "-t"}, false, topol);
+	parser.AddOption({ "-simparams", "-s" }, false, simpar);
+	parser.AddOption({ "-conf_out", "-co" }, false, conf_out);
+	parser.AddOption({ "-trajectory", "-trr", "-traj"}, false, trajOut);
+	parser.AddFlag({ "-display", "-d" }, [&render]() { render = true; });
+
+    EnvMode envmode = render ? Full : ConsoleOnly;
+
+    auto env = std::make_unique<Environment>(work_dir, envmode);
+
+    const SimParams ip(simpar);
+    GroFile grofile{ conf };
+    TopologyFile topfile{ topol };
 
     if ((NodeIndex(grofile.box_size.ToInt3()).toFloat3() - grofile.box_size).len() > 0.0001 || grofile.box_size.x != grofile.box_size.y || grofile.box_size.y != grofile.box_size.z) {
         const int newSize = std::ceil(std::max(std::max(grofile.box_size.x, grofile.box_size.y), grofile.box_size.z));
@@ -142,10 +98,10 @@ int mdrun(int argc, char** argv) {
     const std::chrono::duration<double> enginetime = env->run(); // [s]
     printf("Engine time %f\n", enginetime.count());
     env->WriteBoxCoordinatesToFile(grofile);
-    grofile.printToFile(setup.conf_out);
+    grofile.printToFile(conf_out);
 
-    if (setup.trajOut.has_value()) {
-        env->getSimPtr()->ToTracjectoryFile()->Dump(setup.trajOut.value());
+    if (!trajOut.empty()) {
+        env->getSimPtr()->ToTracjectoryFile()->Dump(trajOut);
     }
 
     // Calculate total time simulated (in nanoseconds)
